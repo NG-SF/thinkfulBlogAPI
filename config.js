458df5e9a3f0fe6307e1d2cd87a3914c667a4786
Nataliya Gapochka <natasha.gapochka@gmail.com>
2018-01-08 16:22:07 -0800

    add link to mLab url

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/restful_blog_app';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/restful_blog_app' || "mongodb://letmein:333999@ds237717.mlab.com:37717/restful_blog_db";
+
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/restaurants-app';
+
 exports.PORT = process.env.PORT || 8080;
 
